91108693bb38ae49599992b13dcfe8926b47fe10
Pedro <pedromarques391@gmail.com>
2023-12-16 15:44:12 -0300

    style: Ajuste no estilo do formulário

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,6 @@ function Footer() {
   const [emptyName, setEmptyName] = useState(false);
   const [emptyMessage, setEmptyMessage] = useState(false);
 
-
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const handleClick = (() => {
@@ -31,45 +30,45 @@ function Footer() {
   };
 
   return (
-    <footer className={`bg-white dark:bg-black mt-7 border-t-2 border-slate-500 text-black dark:text-white flex justify-center md:mt-10 md:w-4/5 md:mx-auto`}>
+    <footer className="bg-white dark:bg-black mt-7 border-t-2 border-slate-500 text-black dark:text-white flex justify-center md:mt-10 md:w-4/5 md:mx-auto">
       <section className="flex flex-col md:flex-row justify-between w-4/5 mt-5 tablet:flex-wrap pb-1">
-        
-          <form onSubmit={handleFormSubmit} className="flex flex-col mb-5 p-5 justify-between h-52 order-1 md:order-2 md:w-2/5 md:mb-10">
-            <h1 className='text-center md:text-xl mb-8 uppercase'>Me envie uma mensagem!</h1>
-            <div>
-              <label htmlFor="name" />
-              <input
-                value={name}
-                id="name"
-                onChange={(e) => setName(e.target.value)}
-                className={`mb-2 w-full h-8 p-2 bg-transparent border-b-2 placeholder:uppercase placeholder:text-black placeholder:dark:text-white focus:outline-none md:mb-10 border-black dark:border-white ${emptyName && 'border-b-2 border-red-600 dark:border-red-600 transition duration-300 shake'}`}
-                type="text"
-                placeholder="Nome"
-              />
 
-            </div>
+        <form onSubmit={handleFormSubmit} className="flex flex-col mb-5 p-5 justify-between h-52 order-1 md:order-2 md:w-2/5 md:mb-10">
+          <h1 className="text-center md:text-xl mb-8 uppercase">Me envie uma mensagem!</h1>
+          <div>
+            <label />
+            <input
+              value={name}
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+              className={`mb-2 w-full h-8 p-2 bg-transparent border-b-2 placeholder:uppercase placeholder:text-black placeholder:dark:text-white focus:outline-none md:mb-10 border-black dark:border-white ${emptyName && ' border-red-600 dark:border-red-700 transition duration-300 shake'}`}
+              type="text"
+              placeholder="Nome"
+            />
 
-            <div>
-              <input
-                value={message}
-                id="message"
-                onChange={(e) => setMessage(e.target.value)}
-                className={`mb-2 w-full h-8 p-2 bg-transparent border-b-2 placeholder:uppercase focus:outline-none md:mb-5 placeholder:text-black placeholder:dark:text-white border-black dark:border-white ${emptyMessage && 'border-b-2 border-red-600 dark:border-red-600 transition duration-300 shake'}`}
-                type="text"
-                placeholder="Mensagem"
-              />
-
-            </div>
-            <button type="submit" className="bg-blue-600 dark:bg-orange-600 dark:hover:bg-orange-700 text-white tracking-widest text-xl text-center py-2 mt-2 rounded-xl hover:bg-blue-900 duration-1000">Enviar</button>
-          </form>
-          <div className='flex flex-col items-center order-2 md:order-1 md:w-2/5'>
-          <Logo/>
-            <ul className='w-full flex justify-around my-5 md:p-5'>
-                <li className='text-3xl md:text-5xl md:p-10 p-5 hover:text-rose-700 duration-1000'><a href=""><i className="fa-brands fa-instagram"></i></a></li>
-                <li className='text-3xl md:text-5xl md:p-10 p-5 hover:text-blue-600 dark:hover:text-orange-600 duration-1000'><a href=""><i className="fa-brands fa-github"></i></a></li>
-                <li className='text-3xl md:text-5xl md:p-10 p-5 hover:text-orange-700 dark:hover:text-blue-700 duration-1000'><a href=""><i className="fa-brands fa-x"></i></a></li>
-            </ul>
           </div>
+
+          <div>
+            <input
+              value={message}
+              id="message"
+              onChange={(e) => setMessage(e.target.value)}
+              className={`mb-2 w-full h-8 p-2 bg-transparent border-b-2 placeholder:uppercase focus:outline-none md:mb-5 placeholder:text-black placeholder:dark:text-white border-black dark:border-white ${emptyMessage && 'border-red-600 dark:border-red-700 transition duration-300 shake'}`}
+              type="text"
+              placeholder="Mensagem"
+            />
+
+          </div>
+          <button type="submit" className="bg-blue-600 dark:bg-orange-600 dark:hover:bg-orange-700 text-white tracking-widest text-xl text-center py-2 mt-2 rounded-xl hover:bg-blue-900 duration-1000">Enviar</button>
+        </form>
+        <div className="flex flex-col items-center order-2 md:order-1 md:w-2/5">
+          <Logo />
+          <ul className="w-full flex justify-around my-5 md:p-5">
+            <li className="text-3xl md:text-5xl md:p-10 p-5 hover:text-rose-700 duration-1000"><a href="https://www.instagram.com/pedromarques.py/"><i className="fa-brands fa-instagram" /></a></li>
+            <li className="text-3xl md:text-5xl md:p-10 p-5 hover:text-blue-600 dark:hover:text-orange-600 duration-1000"><a href="https://github.com/PedroMarques391"><i className="fa-brands fa-github" /></a></li>
+            <li className="text-3xl md:text-5xl md:p-10 p-5 hover:text-orange-700 dark:hover:text-blue-700 duration-1000"><a href="https://www.linkedin.com/in/pedromarques391/"><i className="fa-brands fa-linkedin" /></a></li>
+          </ul>
+        </div>
       </section>
     </footer>
   );
